508cea5dabb9d13d820ca2f02c35ac4a2f1d3166
Ivanciu Ionut <ionutziv7@protonmail.com>
2022-04-29 16:08:12 +0300

    Added variables

---
+++ Code.with.Mosh/The Ultimate JavaScript Mastery Series/2.Basics/1- Variables/index.js
@@ -0,0 +1,11 @@
+let name = 'John';
+console.log(name);
+
+// Cannot be a reserved keyword
+// Should be meaningful
+// Cannot start with a number (1name) 
+// Cannot contain a space or a hyphen (-)
+// Are case-sensitive
+
+let firstName = 'John';
+let lastName = 'I';
