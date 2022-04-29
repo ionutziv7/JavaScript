4a8ba51169dbb966df0e5e3980c77f9655475062
Ivanciu Ionut <ionutziv7@protonmail.com>
2022-04-29 20:29:12 +0300

    Added Functions

---
+++ Code.with.Mosh/The Ultimate JavaScript Mastery Series/2.Basics/7- Functions/index.js
@@ -0,0 +1,5 @@
+function greet(name, lastName) { // name is a parameter
+    console.log('Hi ' + name + ' ' + lastName);
+}
+
+greet('John', 'Smith');  // 'John' is an argument, the actual value we suply for the name 
