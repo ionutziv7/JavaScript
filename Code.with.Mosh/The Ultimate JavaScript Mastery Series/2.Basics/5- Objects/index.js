b805b71fb662083c4c1ea1aafd6124471f8d7953
Ivanciu Ionut <ionutziv7@protonmail.com>
2022-04-29 20:01:07 +0300

    Added Objects

---
+++ Code.with.Mosh/The Ultimate JavaScript Mastery Series/2.Basics/5- Objects/index.js
@@ -0,0 +1,13 @@
+// Object Literal
+let person =  {
+    name: 'John',
+    age: 26
+};
+
+// Dot Notation
+person.name = 'Johnny';
+
+// Bracket Notation
+let selection = 'name';
+person[selection] = 'Mary';
+console.log(person.name);
