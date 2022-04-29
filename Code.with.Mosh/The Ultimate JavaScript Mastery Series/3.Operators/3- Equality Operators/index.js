f9536d89adca57fa9b35c13d20db79f67c1a0061
Ivanciu Ionut <ionutziv7@protonmail.com>
2022-04-29 21:00:39 +0300

    More Operators

---
+++ Code.with.Mosh/The Ultimate JavaScript Mastery Series/3.Operators/3- Equality Operators/index.js
@@ -0,0 +1,9 @@
+// Strict Equality (Type + Value)
+console.log(1 === 1);
+console.log('1' === 1);
+
+//Loose Equality
+console.log(1 == 1);
+console.log('1' == 1);
+console.log(true == 1);
+console.log(false == 1);
