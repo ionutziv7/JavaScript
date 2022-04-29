d466d1c1ddc0bcff190e436c8d64d9b843d756bb
Ivanciu Ionut <ionutziv7@protonmail.com>
2022-04-29 18:01:42 +0300

    Added Primitive Types

---
+++ Code.with.Mosh/The Ultimate JavaScript Mastery Series/2.Basics/3- Primitive Types/index.js
@@ -0,0 +1,9 @@
+let name = 'John'; // String Literal
+let age = 26; // Number Literal
+let isApproved = false; // Boolean Literal
+let firstName = undefined;
+let selectedColor = 'blue';
+
+console.log("Name is: " + name);
+console.log("Age is: " + age);
+console.log("Selected color: " + selectedColor);
