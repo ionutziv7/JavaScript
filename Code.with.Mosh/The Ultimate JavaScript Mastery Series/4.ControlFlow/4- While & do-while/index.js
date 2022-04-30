a25be9f049e65c8a81b2e71c1d7164990e452eee
Ivanciu Ionut <ionutziv7@protonmail.com>
2022-04-30 15:28:29 +0300

    Added Loops

---
+++ Code.with.Mosh/The Ultimate JavaScript Mastery Series/4.ControlFlow/4- While & do-while/index.js
@@ -0,0 +1,19 @@
+let i = 0;
+while(i <= 5) {
+    if(i % 2 !== 0) console.log(i);
+    i++;
+}
+console.log();
+
+// do-while
+let j = 5;
+do {
+    if(j % 2 !== 0) console.log(j);
+    j--;
+} while(j >= 0);
+console.log();
+let x = 10;
+do {
+    if(x % 2 == 0) console.log(x);
+    x--;
+} while(x <= 5);
---
+++ Code.with.Mosh/The Ultimate JavaScript Mastery Series/4.ControlFlow/5- Infinite Loops/index.js
@@ -0,0 +1,17 @@
+let i= 0;
+while(i < 5) {
+    console.log(i);
+    i++; // without i++ we'll have an infinite loop
+}
+
+while (true) {
+}
+
+let x = 0;
+do {
+    // x++;
+} while(x < 5);
+
+for(let i = 0; i <10;) {
+    // also infinite loop
+}
