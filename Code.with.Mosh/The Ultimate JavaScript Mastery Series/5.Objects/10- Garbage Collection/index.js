baa30272b9c5a3da172ffa1d9f7319c9ddf0602d
Ivanciu Ionut <ionutziv7@protonmail.com>
2022-05-04 18:08:02 +0300

    Garbage Collection

---
+++ Code.with.Mosh/The Ultimate JavaScript Mastery Series/5.Objects/10- Garbage Collection/index.js
@@ -0,0 +1,6 @@
+let circle = {};
+console.log(circle);
+
+// Garbage Collector finds the variables or constants that are no longer 
+// used and then deallocate the memory that was allocate to them earlier
+// Memory allocation and deallocation happends automatically
