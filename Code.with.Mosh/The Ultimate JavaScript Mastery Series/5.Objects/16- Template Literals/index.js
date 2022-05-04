ba170fbd6a7ec4986f49d6db1b263ee457185d72
Ivanciu Ionut <ionutziv7@protonmail.com>
2022-05-04 19:45:33 +0300

    Template Literals

---
+++ Code.with.Mosh/The Ultimate JavaScript Mastery Series/5.Objects/16- Template Literals/index.js
@@ -0,0 +1,25 @@
+const message = 
+'This is my\n'+
+'\'first\' message';
+console.log(message);
+
+// Object {}
+// Boolean true, false
+// String '', ""
+// Template ``
+
+const another = 
+`This is my 
+'first' message`;
+console.log(another);
+
+const name = 'John';
+const greet = 5;
+const mail = 
+`Hi ${greet} ${name},
+
+Thank you for joining my mailing list.
+
+Regards,
+Mosh`;
+console.log(mail);
