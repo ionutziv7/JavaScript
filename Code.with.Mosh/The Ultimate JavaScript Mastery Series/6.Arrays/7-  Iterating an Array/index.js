9e8718202996a315f57e4e0764fff1edc304a532
Ivanciu Ionut <ionutziv7@protonmail.com>
2022-05-05 23:19:13 +0300

    Update iterating an array

--- Code.with.Mosh/The Ultimate JavaScript Mastery Series/6.Arrays/7-  Iterating an Array/index.js
+++ Code.with.Mosh/The Ultimate JavaScript Mastery Series/6.Arrays/7-  Iterating an Array/index.js
@@ -3,6 +3,4 @@ const numbers = [1, 2, 3];
 for(let number of numbers) 
     console.log(number);
 
-numbers.forEach(function(number) {
-    console.log(number);
-});
+numbers.forEach((number, index) => console.log(index, number));
