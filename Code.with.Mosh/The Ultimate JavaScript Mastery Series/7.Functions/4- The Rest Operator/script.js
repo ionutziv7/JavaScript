9413ea08605fa6bf6304843c229403136f8b4cfe
Ivanciu Ionut <ionutziv7@protonmail.com>
2022-05-07 23:28:48 +0300

    The Rest Operator

---
+++ Code.with.Mosh/The Ultimate JavaScript Mastery Series/7.Functions/4- The Rest Operator/script.js
@@ -0,0 +1,7 @@
+// rest operator ... not to be confused with spread operator wich is ... ?!
+function sum(discount, ...prices) {
+   const total =  prices.reduce((a, b => a + b));
+    return total * (1 - discount);
+}
+
+console.log(sum(0.1, 20, 30));
