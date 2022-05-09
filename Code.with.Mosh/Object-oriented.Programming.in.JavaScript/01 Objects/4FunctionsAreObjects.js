ca04885e4b6a9af3570fa8d7ccd98b0ab095bcd7
Ivanciu Ionut <ionutziv7@protonmail.com>
2022-05-09 18:37:24 +0300

    Functions are Objects

---
+++ Code.with.Mosh/Object-oriented.Programming.in.JavaScript/01 Objects/4FunctionsAreObjects.js
@@ -0,0 +1,16 @@
+function Circle(radius) {
+    this.radius = radius;
+    this.draw = function() {
+        console.log('draw');
+    }
+}
+
+const Circle1 = new Function('radius', `this.radius = radius;
+this.draw = function() {
+    console.log('draw');
+}`);
+
+Circle.call({}, 1);
+Circle.apply({}, [1, 2, 3]);
+
+const another = new Circle(1);
