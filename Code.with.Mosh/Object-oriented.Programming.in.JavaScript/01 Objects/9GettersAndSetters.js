a348c17eb93f51b386a2b1c3ffdef779d83f9736
Ivanciu Ionut <ionutziv7@protonmail.com>
2022-05-09 20:40:26 +0300

    Getters and Setters

---
+++ Code.with.Mosh/Object-oriented.Programming.in.JavaScript/01 Objects/9GettersAndSetters.js
@@ -0,0 +1,27 @@
+
+function Circle(radius) {
+    this.radius = radius;
+
+    let defaultLocation = { x: 0, y: 0 };
+
+    this.getDefaultLocation = function() {
+        return defaultLocation;
+    }
+    this.draw = function() {
+        console.log('draw');
+    }
+    Object.defineProperty(this, 'defaultLocation', {
+        get: function() {
+            return defaultLocation;
+        },
+        set: function(value) {
+            if(!value.x || !value.y) // if value.x equals falsy or value.y equals falsy
+                throw new Error('Invalid location.');
+            defaultLocation = value;
+        }
+    });
+}
+
+const circle = new Circle(10);
+circle.defaultLocation = 1;
+circle.draw();
