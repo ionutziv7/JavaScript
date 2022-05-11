895fbaf96afb5b5e6a31f7895061d74eac0a59e2
Ionut Ivanciu <ionutziv7@protonmail.com>
2022-05-11 19:44:42 +0300

    Creating your own prototypical inheritance

---
+++ Code.with.Mosh/Object-oriented.Programming.in.JavaScript/03 Prototypical Inheritance/Creating_own_prototypical.js
@@ -0,0 +1,19 @@
+function Shape() {
+}
+
+Shape.prototype.duplicate = function() {
+    console.log('duplicate');
+}
+
+function Circle(radius) {
+    this.radius = radius;
+}
+
+Circle.prototype = Object.create(Object.prototype);
+
+Circle.prototype.draw = function() {
+    console.log('draw');
+}
+
+const s = new Shape();
+const c = new Circle(1);
