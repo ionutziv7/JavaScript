2a1ec2022e7804bdfb38185b0a14b0eed5b1d4da
Ionut Ivanciu <ionutziv7@protonmail.com>
2022-05-12 22:05:40 +0300

    Private Members using WeakMap

---
+++ Code.with.Mosh/Object-oriented.Programming.in.JavaScript/04 ES6 Classes/Private Members using WeakMaps.js
@@ -0,0 +1,21 @@
+
+const _radius = new WeakMap();
+const _move = new WeakMap();
+
+class Circle {
+    constructor(radius) {
+        _radius.set(this, radius);
+
+        _move.set(this, () => {
+            console.log('move', this);
+        });
+    }
+   
+    draw() {
+        _move.get(this)();
+
+        console.log('draw');
+    }
+}
+
+const c = new Circle(1);
